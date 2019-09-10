50a289522e0d611886128469960e21a477c8716f
sixfwa <farhanaliw6@gmail.com>
2019-09-11 01:39:54 +0200

    Update/Delete/Detail

---
+++ articles/views.py
@@ -0,0 +1,23 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Article
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ('title', 'body',)
+    template_name = 'article_edit.html'
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
